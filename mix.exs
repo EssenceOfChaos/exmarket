aad9fad0b9e16cd78dfdb26133848d4afde696a0
EssenceOfChaos <fjschiller@gmail.com>
2019-04-22 22:24:40 -0400

    published package to Hex

--- mix.exs
+++ mix.exs
@@ -3,14 +3,18 @@ defmodule Exmarket.MixProject do
   Application entry point and dependency management
   """
   use Mix.Project
+  @version "0.1.0"
 
   def project do
     [
       app: :exmarket,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      aliases: aliases(),
+      description: description(),
+      package: package(),
       ## Docs ##
       name: "ExMarket",
       source_url: "https://github.com/EssenceOfChaos/exmarket",
@@ -20,7 +24,7 @@ defmodule Exmarket.MixProject do
     ]
   end
 
-  def package do
+  defp package do
     [
       name: :exmarket,
       maintainers: ["Frederick John"],
@@ -46,5 +50,21 @@ defmodule Exmarket.MixProject do
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
+  end
+
+  defp aliases do
+    [
+      clean: [&clean_test_fixtures/1, "clean"],
+      build: ["deps.get"],
+      setup: []
+    ]
+  end
+
+  defp description() do
+    "ExMarket is a Hex Package designed to provide a clean interface for retrieving real-time stock and crypto data."
+  end
+
+  defp clean_test_fixtures(_args) do
+    File.rm_rf("test/tmp")
   end
 end
